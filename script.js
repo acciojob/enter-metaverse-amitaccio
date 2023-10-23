4a9b51808da7a48535b15f49780811974067f36c
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-10-23 09:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let para= document.getElementById("status");
+let btn= document.getElementById("enterBtn");
+
+
+button.addEventListener("click" ,() =>{
+	let heading= document.createElement("h1");
+	heading.id = "enterBtn";
+	heading.innerText = "Entered Metaverse";
+	document.body.insertBefore(heading , para);
+	para.remove();
+})
