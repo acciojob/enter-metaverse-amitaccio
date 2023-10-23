a0a036a4d00988b254e277e9919d2d44d764fadb
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-10-23 09:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let para= document.getElementById("status");
 let btn= document.getElementById("enterBtn");
 
 
-button.addEventListener("click" ,() =>{
-	let heading= document.createElement("h1");
-	heading.id = "enterBtn";
-	heading.innerText = "Entered Metaverse";
-	document.body.insertBefore(heading , para);
+button.addEventListener("click", () =>{
+	let h1= document.createElement("h1");
+	h1.id = "status";
+	h1.innerText = "Entered Metaverse";
+	document.body.insertBefore(h1 , para);
 	para.remove();
 })
