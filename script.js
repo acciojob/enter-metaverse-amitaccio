242b3fd2d4d1e5aefb3a3e77bab78a73686e072e
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-21 12:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 function changeContent () {
 	let p = document.getElementById("status");
     let newElm = document.createElement("h1");
-	p.innerText = newElm;
-	p.innerText = "Entered Metaverse";
+	newElm.id = "status";
+	newElm.innerText = "Entered Metaverse";
+	p.innerText = "";
+	p.appendChild(newElm);
 
 }
 
